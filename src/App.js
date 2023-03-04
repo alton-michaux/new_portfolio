4f0c3e377c9545a68c7d3951c15edeb47c0cccbd
Alton Michaux <alton.michaux@yahoo.com>
2023-03-03 22:36:07 -0500

    creating project files/structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home"
+import About from "./routes/About"
+import Contact from "./routes/Contact"
+import Experience from "./routes/Experience"
+
+import './css/App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
