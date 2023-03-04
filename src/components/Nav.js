99fa4891c25bcf0b070b6e0cbcb4fe40d8bb916c
Alton Michaux <alton.michaux@yahoo.com>
2023-03-04 11:03:47 -0500

    nav links are working

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,12 +2,12 @@ import styles from "../css/Nav.module.css"
 
 const Nav = () => {
   return (
-    <div className="nav-bar container-fluid">
-      <ul className="nav-list container-fluid d-inline-flex">
-        <li className="list-items p-3 d-flex col-3 justify-content-around"><a href="Resume/resume.html">Experience</a></li>
-        <li className="list-items p-3 d-flex col-3 justify-content-around"><a href="About/about.html">About</a></li>
-        <li className="list-items p-3 d-flex col-3 justify-content-around"><a href="Contact/contact.html">Contact</a></li>
-        <li className="list-items p-3 d-flex col-3 justify-content-around"><a href="./Docs/Alton_Michaux_Resume.docx" download>Resume</a></li>
+    <div className={[styles.navBar, 'container-fluid']}>
+      <ul className={[styles.navList, "container-fluid", "d-inline-flex"]}>
+        <li className={[styles.listItems, "p-3 d-flex", "col-3", "justify-content-around"]}><a href="/experience">Experience</a></li>
+        <li className={[styles.listItems, "p-3 d-flex", "col-3", "justify-content-around"]}><a href="/about">About</a></li>
+        <li className={[styles.listItems, "p-3 d-flex", "col-3", "justify-content-around"]}><a href="/contact">Contact</a></li>
+        <li className={[styles.listItems, "p-3 d-flex", "col-3", "justify-content-around"]}><a href="./Docs/Alton_Michaux_Resume.docx" download>Resume</a></li>
       </ul>
     </div>
   )
