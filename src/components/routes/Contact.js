09aa342590fe98df11c8aa1d3731624d36a4ac7e
Alton Michaux <alton.michaux@yahoo.com>
2023-03-04 00:40:46 -0500

    fixed issue with about page, still migrating html

--- src/components/routes/Contact.js
+++ src/components/routes/Contact.js
@@ -7,6 +7,16 @@ const Contact = () => {
   return (
     <>
       <Nav></Nav>
+      <div class="main-page-greet-box container-fluid text-center">
+        <h1 class="greet">Let's Connect.</h1>
+        <div class="greet">
+          <ul class="container">
+            <li class="contact-items list-group-item"><a href="mailto:alton.michaux@yahoo.com" target="none">Email</a></li>
+            <li class="contact-items list-group-item"><a href="https://github.com/alton-michaux" target="none">GitHub</a></li>
+            <li class="contact-items list-group-item"><a href="https://www.linkedin.com/in/alton-michaux-114b941ba/" target="none">LinkedIn</a></li>
+          </ul>
+        </div>
+      </div>
       <Footer></Footer>
     </>
   )
