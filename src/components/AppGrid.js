ebda5c976f73a2479d9e072078e8ff6ad2298476
Alton Michaux <alton.michaux@yahoo.com>
2023-03-05 00:36:54 -0500

    moved images and links into an array which is mapped onto the page

--- src/components/AppGrid.js
+++ src/components/AppGrid.js
@@ -3,72 +3,84 @@ import navStyles from "../css/Nav.module.css"
 import styles from "../css/AppGrid.module.css"
 
 const AppGrid = () => {
+  const appListItems =
+    [
+      {
+        href: "https://gvomusicdiscovery.netlify.app/",
+        imgSrc: "https://idsb.tmgrup.com.tr/ly/uploads/images/2020/10/12/64723.jpg",
+        imgAlt: "spotify playlist app",
+        text: "Spotify Playlist App (JS)",
+        key: 1
+      }, {
+        href: "https://silly-reddit-stage.herokuapp.com/",
+        imgSrc: "https://helios-i.mashable.com/imagery/articles/06ce8F6eXTFuryavAjP7fuY/hero-image.fill.size_1200x1200.v1623369766.jpg",
+        imgAlt: "Reddit clone",
+        text: "Reddit Clone App (RoR)",
+        key: 2
+      }, {
+        href: "https://chorbee.herokuapp.com/parents/sign_in",
+        imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDP7Dc75L_m9zazv1Kb4rskgQfP9FofKc3tg&usqp=CAU",
+        imgAlt: "chorbee web app",
+        text: "Chorebee Web App (RoR)",
+        key: 3
+      }
+    ]
+
+    const gameListItems =
+    [
+      {
+        href: "https://alton-michaux.github.io/Breakout-game-/",
+        imgSrc: "https://image.api.playstation.com/vulcan/ap/rnd/202009/1415/6nNsvW4EX4wGbcIItg0paDEV.jpg?w=1024",
+        imgAlt: "breakout game",
+        text: "Breakout (JS)",
+        key: 1
+      }
+    ]
   return (
     <div className={styles.canvasGrid}>
       <div className={styles.canvasDiv}>
         <div className={[styles.projectDisplay]} id="appDiv">
-          <a
-            className={styles.projectLink}
+          {appListItems.map((item) => {
+            return (
+              <a
+                className={styles.projectLink}
 
-            href="https://gvomusicdiscovery.netlify.app/"
-            target="blank"
-          >
-            <div className={styles.appBlock} id="apps">
-              <img
-                src="https://idsb.tmgrup.com.tr/ly/uploads/images/2020/10/12/64723.jpg"
-                alt="spotify playlist app"
-                className={globalStyles.thumbnail}
-              />
-              <div className={globalStyles.text}>Spotify Playlist App (JS)</div>
-            </div>
-          </a>
-          <a
-            className={styles.projectLink}
-            href="https://silly-reddit-stage.herokuapp.com/"
-            target="blank"
-          >
-            <div className={styles.appBlock} id="apps">
-              <img
-                src="https://helios-i.mashable.com/imagery/articles/06ce8F6eXTFuryavAjP7fuY/hero-image.fill.size_1200x1200.v1623369766.jpg"
-                alt="Reddit clone"
-                className={globalStyles.thumbnail}
-              />
-              <div className={globalStyles.text}>Reddit Clone App (RoR)</div>
-            </div>
-          </a>
-          <a
-            className={styles.projectLink}
-
-            href="https://chorbee.herokuapp.com/parents/sign_in"
-            target="blank"
-          >
-            <div className={styles.appBlock} id="apps">
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDP7Dc75L_m9zazv1Kb4rskgQfP9FofKc3tg&usqp=CAU"
-                alt="chorbee web app"
-                className={globalStyles.thumbnail}
-              />
-              <div className={[globalStyles.text, navStyles.listItems].join(' ')}>Chorebee Web App (RoR)</div>
-            </div>
-          </a>
+                href={item.href}
+                target="blank"
+              >
+                <div className={styles.appBlock} id="apps">
+                  <img
+                    src={item.src}
+                    alt={item.alt}
+                    className={globalStyles.thumbnail}
+                  />
+                  <div className={item.key === 3 ? [globalStyles.text, navStyles.listItems].join(' ') : globalStyles.text}>{item.text}</div>
+                </div>
+              </a>
+            )
+          })}
         </div>
       </div>
       <div className={styles.canvasDiv}>
         <div className={styles.projectDisplay} id="gameDiv">
-          <a
-            className={styles.projectLink}
-            href="https://alton-michaux.github.io/Breakout-game-/"
-            target="blank"
-          >
-            <div className={[styles.gameBlock, styles.appBlock].join(' ')} id="games">
-              <img
-                src="https://image.api.playstation.com/vulcan/ap/rnd/202009/1415/6nNsvW4EX4wGbcIItg0paDEV.jpg?w=1024"
-                alt="breakout game"
-                className={globalStyles.thumbnail}
-              />
-              <div className={[globalStyles.text, navStyles.listItems].join(' ')}>Breakout Game (JS)</div>
-            </div>
-          </a>
+          {gameListItems.map((item) => {
+            return (
+              <a
+                className={styles.projectLink}
+                href={item.href}
+                target="blank"
+              >
+                <div className={[styles.gameBlock, styles.appBlock].join(' ')} id="games">
+                  <img
+                    src={item.src}
+                    alt={item.alt}
+                    className={globalStyles.thumbnail}
+                  />
+                  <div className={[globalStyles.text, navStyles.listItems].join(' ')}>{item.text}</div>
+                </div>
+              </a>
+            )
+          })}
         </div>
       </div>
     </div>
