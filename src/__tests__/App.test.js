8c751853ef7610faf414673c257b1278241a0a20
Alton Michaux <alton.michaux@yahoo.com>
2023-03-12 11:00:27 -0400

    added test for about page

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -6,16 +6,30 @@ import '@testing-library/jest-dom'
 import App from '../App'
 import { BrowserRouter, MemoryRouter } from 'react-router-dom'
 
-test('full app rendering/navigating', async () => {
+test('landing page', async () => {
+  const user = userEvent.setup()
   render(<App />, { wrapper: BrowserRouter })
-  // const user = userEvent.setup()
 
   // verify page content for default route
   expect(screen.getByText(/Hey, I'm Alton and I Develop Web Apps./i)).toBeInTheDocument()
 
   // verify page content for expected route after navigating
-  // await user.click(screen.getByText(/about/i))
+  // await user.click(screen.getByText(/About/i))
   // expect(screen.getByText(/As a back end developer/i)).toBeInTheDocument()
+})
+
+test('about page', () => {
+  const aboutRoute = '/about'
+
+  // use <MemoryRouter> when you want to manually control the history
+  render(
+    <MemoryRouter initialEntries={[aboutRoute]}>
+      <App />
+    </MemoryRouter>,
+  )
+
+  // verify navigation to "no match" route
+  expect(screen.getByText(/ORM and RESTful APIs/i)).toBeInTheDocument()
 })
 
 test('landing on a bad page', () => {
